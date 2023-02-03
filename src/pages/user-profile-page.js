d55cbf5a83042361b07a8680996e49a14252b81f
dshklr <dariashkliar@seznam.cz>
2023-02-03 13:38:47 +0200

    add react-modal

--- src/pages/user-profile-page.js
+++ src/pages/user-profile-page.js
@@ -3,12 +3,15 @@ import { useParams } from "react-router-dom";
 import Loading from "../components/loader/loader";
 import { api } from "../api/index";
 import UserProfileCard from "../components/user-profile-card/user-profile-card";
+import { useNavigate } from "react-router-dom";
+import ConfirmModal from "../components/confirm-modal/confirm-modal";
 
 function UserPage() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [company, setCompany] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,17 +33,48 @@ function UserPage() {
     fetchData();
   }, [id]);
 
+  const navigate = useNavigate();
+
+  function handleConfirm() {
+    navigate(-1);
+  }
+
+  const handleDelete = () => {
+    setShowModal(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    try {
+      await api.delete(`/users/${id}`);
+      handleConfirm();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
       {isLoading ? (
         <Loading />
       ) : (
-        <UserProfileCard company={company} user={user} />
+        <UserProfileCard
+          handleDelete={handleDelete}
+          company={company}
+          user={user}
+        />
       )}
+
+      <ConfirmModal
+        isOpen={showModal}
+        onClose={handleClose}
+        onConfirm={handleConfirmDelete}
+      />
     </div>
   );
 }
 
 export default UserPage;
-
-
